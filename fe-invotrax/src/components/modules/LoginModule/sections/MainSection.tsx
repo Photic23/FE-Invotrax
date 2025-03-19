81c126cafbc0c24785d1d6e4cfc3266c749053db
naufal.mahdy <naufal.mahdy@ui.ac.id>
2025-03-19 09:42:41 +0700

    initial set for login page

---
+++ fe-invotrax/src/components/modules/LoginModule/sections/MainSection.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainSection = () => {
+    return (
+        <div className='w-screen scroll-smooth'>
+            <MainSection/>
+        </div>
+    );
+}
+
+export default MainSection;
