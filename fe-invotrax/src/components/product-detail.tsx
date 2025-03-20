c64e456bce3cb0e9cdf2a9bc22bf5e6b87ea5d3b
DevinFaiz <devinfaiz29@gmail.com>
2025-03-20 16:19:27 +0700

    feat: revisi button kembali

--- fe-invotrax/src/components/product-detail.tsx
+++ fe-invotrax/src/components/product-detail.tsx
@@ -133,7 +133,7 @@ export default function ProductDetail({ productId }: ProductDetailProps) {
     <Card className="max-w-2xl mx-auto">
       <CardHeader className="relative">
         <button
-          onClick={() => router.back()}
+          onClick={() => router.push("/produk")}
           className="absolute left-4 top-4 flex items-center text-gray-600 hover:text-blue-800 transition"
         >
           <ArrowLeft className="w-5 h-5 mr-2" />
